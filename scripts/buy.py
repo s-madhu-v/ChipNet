b2737aab3507f840a07537d5a74bf63d5097f217
imadhui <xpdbymadhu+projects@gmail.com>
2023-04-12 00:19:17 +0530

    merge

---
+++ scripts/buy.py
@@ -0,0 +1,42 @@
+from brownie import accounts, ChipNet, web3
+import time
+import os
+
+
+def myFunc():
+    print("My Func")
+
+
+# write a function to post an ad
+def postAd(name):
+    contractAddress = os.getenv("CONTRACT_ADDRESS")
+    chipNet = ChipNet.at(contractAddress)
+    sellerAccount = accounts[2]
+    txn = chipNet.postAd(
+        name,
+        2 * (10**18),
+        {"from": sellerAccount},
+    )
+    txn.wait(1)
+
+
+# write a function to buy an ad
+def buyAd():
+    contractAddress = os.getenv("CONTRACT_ADDRESS")
+    chipNet = ChipNet.at(contractAddress)
+    buyerAccount = accounts[1]
+    sellerAccount = accounts[2]
+    txn = chipNet.purchaseAd(
+        chipNet.getAdsCount() - 1, {"from": buyerAccount, "value": 2 * (10**18)}
+    )
+    txn.wait(1)
+    print(f"Buyer balance: {web3.fromWei(buyerAccount.balance(), 'ether')} ETH")
+    print(f"Seller balance: {web3.fromWei(sellerAccount.balance(), 'ether')} ETH")
+
+
+def main():
+    print("Entered Main")
+    postAd()
+    print("Posted Ad")
+    buyAd()
+    print("Bought Ad")
